d2b22bcc740cc2d51be3d31029bde7c3a0a2781a
Ahrorbek Davronov <axrorbektatu@gmail.com>
2025-02-08 18:09:43 +0500

    new updates

--- app/Services/SubscriptionHistoryCRUDService.php
+++ app/Services/SubscriptionHistoryCRUDService.php
@@ -6,6 +6,8 @@ use App\Http\Resources\SubscriptionHistoryResource;
 use App\Models\Subscription;
 use App\Models\SubscriptionHistory;
 use Carbon\Carbon;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class SubscriptionHistoryCRUDService extends CRUDService
 {
@@ -27,6 +29,35 @@ class SubscriptionHistoryCRUDService extends CRUDService
         $data['end_date'] = $endDate->format('d-m-Y');
         
         $model = $this->modelClass::create($data);
+
+        return successResponse($this->modelResourceClass::make($model));
+    }
+
+
+    public function update(string $id, $data){
+
+        try{
+            $model = $this->modelClass::findOrfail($id);
+            
+            $obuna = Subscription::find($data['subscription_id']);
+
+            // Bugungi sana
+            $startDate = Carbon::today(); // Boshlanish sanasi
+            $endDate = $startDate->copy()->addDays($obuna->period); // Tugash sanasi (bugundan $days kun keyin)
+
+            $data['start_date'] = $startDate->format('d-m-Y');
+            $data['end_date'] = $endDate->format('d-m-Y');
+            
+            $model->update($data);
+            
+        }catch(Exception $e){
+            if ($e instanceof ModelNotFoundException) {
+                return errorResponse("Record not found!");
+            }else{
+                return errorResponse($e->getMessage());
+            }
+        }
+        
 
         return successResponse($this->modelResourceClass::make($model));
     }
