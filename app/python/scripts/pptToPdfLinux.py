e70fbe33fac9ee0e8d26082d72c6418bfe373501
Ahrorbek Davronov <axrorbektatu@gmail.com>
2025-02-14 15:04:26 +0500

    new updates

--- app/python/scripts/pptToPdfLinux.py
+++ app/python/scripts/pptToPdfLinux.py
@@ -12,7 +12,8 @@ def change_extension(input_path):
 def pptx_to_pdf(input_file):
     """ LibreOffice yordamida PPTX faylni PDF formatiga o'tkazish """
     try:
-        subprocess.run(["libreoffice", "--headless", "-env:UserInstallation=file:///tmp/LibreOffice_Conversion_${USER}", "--convert-to", "pdf:writer_pdf_Export", input_file], check=True)
+        # subprocess.run(["libreoffice", "--headless", "-env:UserInstallation=file:///tmp/LibreOffice_Conversion_${USER}", "--convert-to", "pdf:writer_pdf_Export", input_file], check=True)
+        subprocess.run(["unoconv", "-f", "pdf", input_file], check=True)
         print(f"Konvertatsiya muvaffaqiyatli bajarildi: {input_file} -> {change_extension(input_file)}")
     except subprocess.CalledProcessError as e:
         print(f"Xatolik yuz berdi: {e}")
